0f546460218f9af1cde518a31415e6f741852676
Ryu Ishimoto <ryu@midokura.com>
2017-01-23 07:36:19 +0000

    Support for lbaasv2 member polling
    Change-Id: I5220ab492d7ce0c647a08bd9a119771be1c04159

--- midonet_ext/neutron/services/loadbalancer/v2_driver.py
+++ midonet_ext/neutron/services/loadbalancer/v2_driver.py
@@ -15,17 +15,29 @@
 
 # lbaasv2 driver for MidoNet
 
+from neutron import context as ncontext
+from neutron import manager
+
 from oslo_config import cfg
 from oslo_log import helpers as log_helpers
 from oslo_log import log as logging
+from oslo_service import loopingcall
 
-from neutron import manager
+from neutron_lbaas.db.loadbalancer import models
 from neutron_lbaas.drivers import driver_base
+from neutron_lbaas.services.loadbalancer import constants as lb_const
 
 from midonet.neutron.client import base as c_base
 from midonet_ext.neutron.common import utils
 
 LOG = logging.getLogger(__name__)
+
+
+MN_STATUS_TO_OPERATING_STATUS = {
+    'ACTIVE': lb_const.ONLINE,
+    'INACTIVE': lb_const.OFFLINE,
+    'NO_MONITOR': lb_const.NO_MONITOR,
+}
 
 
 class MidonetLoadBalancerDriver(driver_base.LoadBalancerBaseDriver):
@@ -39,6 +51,29 @@ class MidonetLoadBalancerDriver(driver_base.LoadBalancerBaseDriver):
         self.member = MidonetMemberManager(self)
         self.health_monitor = MidonetHealthMonitorManager(self)
         self._client = c_base.load_client(cfg.CONF.MIDONET)
+
+        self.member_update_thread = loopingcall.FixedIntervalLoopingCall(
+            self._update_member_status)
+        self.member_update_thread.start(5, initial_delay=None,
+                                        stop_on_exception=False)
+
+        self.admin_ctx = ncontext.get_admin_context()
+
+    def _update_member_status(self):
+        # Request a list of pool members from the Midonet API
+        # Go through this member list and update the neutron DB with each
+        # pool member's status.
+        db_members = self.plugin.db.get_pool_members(context=self.admin_ctx)
+        for m in db_members:
+            member_id = m.id
+            midonet_member = self._client.get_pool_member(
+                self.admin_ctx, member_id)
+            LOG.debug("backend member: %(member)s", {'member': midonet_member})
+            midonet_status = midonet_member['status']
+            new_status = MN_STATUS_TO_OPERATING_STATUS[midonet_status]
+            self.plugin.db.update_status(
+                context=self.admin_ctx, model=models.MemberV2,
+                id=member_id, operating_status=new_status)
 
 
 def _build_func(method, client_method):
